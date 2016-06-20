368f275a8ff2300bafb1bc874b57c9b308a9f904
Bhawani Parajuli and Cory Olson <bhawani@>
2016-06-20 09:37:18 -0700

    add Pet class

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Pets(breed, age, color, considerations, image, type){
+  this.breed = breed;
+  this.age = age;
+  this.color = color;
+  this.considerations = considerations;
+  this.image = image;
+  this.type = type;
+  this.adopted = false;
+}
+
+Pets.prototype.adopted = function(){
+ (this.adopted) ? this.adopted = false : this.adopted = true;
+}
